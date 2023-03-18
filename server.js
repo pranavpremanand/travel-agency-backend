efe22206e420747d1b14ffdbd5a0b29e26a079ae
Pranav Premanand <mpranavprem@gmail.com>
2023-03-18 09:40:11 +0530

    working on 3rd page

--- server.js
+++ server.js
@@ -2,20 +2,21 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import route from "./routes/route.js";
-
 dotenv.config();
+import route from "./routes/route.js";
+import dbConnection from './config/dbConnection.js'
+
 const app = express();
 // app.use(cors({ origin: "http://localhost:3000" }));
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("Database connected successfully");
-    app.listen(5000, () => console.log(`Server is running on port:${5000}`));
-  })
-  .catch(() => console.log("Database connection failed"));
+// mongoose
+//   .connect(process.env.MONGO_URL)
+//   .then(() => {
+//     console.log("Database connected successfully");
+// })
+// .catch(() => console.log("Database connection failed"));
 app.use("/", route);
+app.listen(5000, () => console.log(`Server is running on port:${5000}`));
 export default app;
