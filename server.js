329d90c1f4a678c1ddcb635b8b485b47b1ae84dd
Pranav Premanand <mpranavprem@gmail.com>
2023-03-21 04:36:30 +0530

    Project done

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 import route from "./routes/route.js";
@@ -11,12 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// mongoose
-//   .connect(process.env.MONGO_URL)
-//   .then(() => {
-//     console.log("Database connected successfully");
-// })
-// .catch(() => console.log("Database connection failed"));
 app.use("/", route);
 app.listen(5000, () => console.log(`Server is running on port:${5000}`));
 export default app;
